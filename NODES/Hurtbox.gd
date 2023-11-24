a3d309dab274788afd51558c6514d362d2346f36
Gallowzz <xtc.claudio@gmail.com>
2023-11-23 19:22:06 -0600

    Damage Fix

--- NODES/Hurtbox.gd
+++ NODES/Hurtbox.gd
@@ -9,14 +9,19 @@ func _init():
 
 func _ready():
 	area_entered.connect(self._on_area_entered)
+	area_exited.connect(self._on_area_exited)
 	
+func _on_area_exited(hitbox: Hitbox):
+	if owner.has_method("player_damaged") && !hitbox.owner is Bala2:
+		hitbox.timer.stop()
+
 func _on_area_entered(hitbox: Hitbox):
 	if owner.has_method("player_damaged") && !hitbox.owner is Bala2:
 		damage = hitbox.damage
 		hitbox.timer.timeout.connect(self._on_timeout)
 		hitbox.timer.start(hitbox.cooldown)
 	
-	if owner.has_method("damaged"):
+	if owner.has_method("damaged") && hitbox.owner is Bala2:
 		owner.damaged(hitbox.damage)
 		
 func _on_timeout():
